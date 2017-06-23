17baf52a544a497691323946c46ec38f339f9f7b
baurine <2008.hbl@gmail.com>
2017-06-23 18:37:46 +0800

    Codelab - Prepare

---
+++ RoomSample/app/src/main/java/com/baurine/roomsample/UsersActivity.java
@@ -0,0 +1,13 @@
+package com.baurine.roomsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+}
